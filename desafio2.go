b1995ee193be5b092e8d805095ea18d943ac9532
JEFFERSON MIGUEL DALLALIBERA <83378566+JeffersonDallalibera@users.noreply.github.com>
2025-12-16 14:28:20 -0300

    Add program to print numbers divisible by 3

---
+++ desafio2.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Números divisíveis por 3 entre 1 e 100:")
+
+    for i := 1; i <= 100; i++ {
+        if i%3 == 0 {
+            fmt.Println(i)
+        }
+    }
+}
